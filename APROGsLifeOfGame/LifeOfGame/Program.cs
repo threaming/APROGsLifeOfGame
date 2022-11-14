65694a04b1441ede7707a403c2ff3692b268cc96
Joel the magical Wizard <joel.vonrotz@hotmail.com>
2022-11-14 19:25:01 +0100

    added the rules and created Cell class

--- APROGsLifeOfGame/LifeOfGame/Program.cs
+++ APROGsLifeOfGame/LifeOfGame/Program.cs
@@ -1,11 +1,36 @@
 ﻿using System;
+
+
+
+/* APROG's
+ *    ______                              ____   __    _ ____   
+ *   / ____/___ _____ ___  ___     ____  / __/  / /   (_) __/__ 
+ *  / / __/ __ `/ __ `__ \/ _ \   / __ \/ /_   / /   / / /_/ _ \
+ * / /_/ / /_/ / / / / / /  __/  / /_/ / __/  / /___/ / __/  __/
+ * \____/\__,_/_/ /_/ /_/\___/   \____/_/    /_____/_/_/  \___/ 
+ *
+ * [Rules of the Game of Life]
+ * In the Game of Life each grid cell can have either one of two states: dead or alive.
+ * The Game of Life is controlled by four simple rules which are applied to each grid cell
+ * in the simulation domain:
+ * 
+ * - A live cell dies if it has fewer than two live neighbors.
+ * - A live cell with two or three live neighbors lives on to the next generation.
+ * - A live cell with more than three live neighbors dies.
+ * - A dead cell will be brought back to live if it has exactly three live neighbors.
+ *
+ * -> Source (https://beltoforion.de/en/game_of_life/)
+ */
+
+
 namespace LifeOfGame
 {
-    internal class Program
+  internal class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+
+      Console.WriteLine("Hello, World!");
     }
+  }
 }
